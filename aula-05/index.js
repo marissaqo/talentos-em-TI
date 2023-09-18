dc2766700629262aebd2a152358e377e2fa1ac73
Marissa Quaresma de Oliveira <141836474+marissaqo@users.noreply.github.com>
2023-09-18 11:57:15 -0300

    Add files via upload

---
+++ aula-05/index.js
@@ -0,0 +1,11 @@
+// console.log('olá mundo');
+
+const nome = prompt("qual seu nome");
+const idade = Number(prompt(`Qual sua idade ${nome}?`));
+const bollean = false;
+
+console.log(`Olá ${nome}, voce tem ${idade} anos, bollean é ${bollean}`);
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof bollean);
